46be812163e8d57fac60d509209c5b9db88d944c
whyspaceee <mohammad.abdan.syakura@mail.ugm.ac.id>
2022-11-25 23:24:23 +0700

    fixed bug with incorrect placement of positioned widget

--- lib/presentation/contribute_screen/components/aiprompt.dart
+++ lib/presentation/contribute_screen/components/aiprompt.dart
@@ -74,80 +74,85 @@ class _AIPromptState extends State<AIPrompt> {
               }
             },
           ),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: Positioned(
+          Positioned(
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
                 child: BackdropFilter(
-              filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                alignment: Alignment.center,
-                height: 100,
-                color: Color.fromRGBO(44, 44, 44, 0.6),
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Generate Image using OpenAI',
-                        style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.white,
-                            fontWeight: FontWeight.w600),
-                      ),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      Container(
-                        height: 48,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Expanded(
-                                child: TextField(
-                              controller: controller,
-                              style: TextStyle(fontSize: 14),
-                              decoration: InputDecoration(
-                                  contentPadding: EdgeInsets.symmetric(
-                                    horizontal: 16,
-                                  ),
-                                  filled: true,
-                                  fillColor: Colors.white,
-                                  border: OutlineInputBorder(
-                                      borderSide: BorderSide.none,
-                                      borderRadius: BorderRadius.circular(20)),
-                                  hintText: 'Describe your flora or fauna'),
-                            )),
-                            SizedBox(width: 8),
-                            IconButton(
-                                iconSize: 28,
-                                style: ButtonStyle(
-                                    shape: MaterialStateProperty.all<
-                                            RoundedRectangleBorder>(
-                                        RoundedRectangleBorder(
-                                            borderRadius:
-                                                BorderRadius.circular(20))),
-                                    fixedSize:
-                                        MaterialStatePropertyAll(Size(64, 64)),
-                                    backgroundColor:
-                                        MaterialStatePropertyAll(yellowText),
-                                    foregroundColor:
-                                        MaterialStatePropertyAll(Colors.white)),
-                                onPressed: () => {
-                                      if (controller.value.text.isNotEmpty)
-                                        {
-                                          context.read<UploadCubit>().getUrl(
-                                              controller.value.text.trim()),
-                                          controller.clear()
-                                        }
-                                    },
-                                icon: Icon(CupertinoIcons.arrow_right))
-                          ],
-                        ),
-                      )
-                    ]),
-              ),
-            )),
-          )
+                  filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    alignment: Alignment.center,
+                    height: 100,
+                    color: Color.fromRGBO(44, 44, 44, 0.6),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Generate Image using OpenAI',
+                            style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w600),
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Container(
+                            height: 48,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Expanded(
+                                    child: TextField(
+                                  controller: controller,
+                                  style: TextStyle(fontSize: 14),
+                                  decoration: InputDecoration(
+                                      contentPadding: EdgeInsets.symmetric(
+                                        horizontal: 16,
+                                      ),
+                                      filled: true,
+                                      fillColor: Colors.white,
+                                      border: OutlineInputBorder(
+                                          borderSide: BorderSide.none,
+                                          borderRadius:
+                                              BorderRadius.circular(20)),
+                                      hintText: 'Describe your flora or fauna'),
+                                )),
+                                SizedBox(width: 8),
+                                IconButton(
+                                    iconSize: 28,
+                                    style: ButtonStyle(
+                                        shape: MaterialStateProperty.all<
+                                                RoundedRectangleBorder>(
+                                            RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(20))),
+                                        fixedSize: MaterialStatePropertyAll(
+                                            Size(64, 64)),
+                                        backgroundColor:
+                                            MaterialStatePropertyAll(
+                                                yellowText),
+                                        foregroundColor:
+                                            MaterialStatePropertyAll(
+                                                Colors.white)),
+                                    onPressed: () => {
+                                          if (controller.value.text.isNotEmpty)
+                                            {
+                                              context
+                                                  .read<UploadCubit>()
+                                                  .getUrl(controller.value.text
+                                                      .trim()),
+                                              controller.clear()
+                                            }
+                                        },
+                                    icon: Icon(CupertinoIcons.arrow_right))
+                              ],
+                            ),
+                          )
+                        ]),
+                  ),
+                )),
+          ),
         ],
       ),
     );
